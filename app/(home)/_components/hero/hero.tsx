4f121d1a287c86922a223fd3508aedb94d79f29b
YZ13 <101273441+YZ13-ENV@users.noreply.github.com>
2024-02-07 16:18:48 +0500

    replace DM Family to Darkmaterial

--- app/(home)/_components/hero/hero.tsx
+++ app/(home)/_components/hero/hero.tsx
@@ -6,25 +6,39 @@ import HeroCard from "./hero-card";
 import StarField from "./star-field";
 
 const Hero = () => {
-
   const HeroContent = () => {
     return (
-      <main style={{ minHeight: 'calc(100dvh - 96px - (18rem / 2))' }}
-      className="w-full h-full max-w-screen-2xl mx-auto md:px-28 px-6 flex flex-col items-center justify-center gap-6">
+      <main
+        style={{ minHeight: "calc(100dvh - 96px - (18rem / 2))" }}
+        className="w-full h-full max-w-screen-2xl mx-auto md:px-28 px-6 flex flex-col items-center justify-center gap-6"
+      >
         <div className="flex flex-col items-center justify-center w-full gap-4 py-2 h-fit">
-          <Image src='/dm-star-dark.svg' width={96} height={96} alt='dm-star-logo' />
+          <Image
+            src="/dm-star-dark.svg"
+            width={96}
+            height={96}
+            alt="dm-star-logo"
+          />
           <h1 className="text-5xl">Darkmaterial</h1>
           <span className="text-lg text-center text-muted-foreground">
-            DM помогает расширить возможности при работе, и поиске вдохновения, <br />
-            здесь вы можете построить свои планы и вдохновиться на новые проекты.
+            Darkmaterial помогает расширить возможности при работе, и поиске
+            вдохновения, <br />
+            здесь вы можете построить свои планы и вдохновиться на новые
+            проекты.
           </span>
         </div>
         <div className="relative flex items-center justify-center w-full gap-4 h-fit">
           <Line className="absolute right-0 w-full -top-4" />
           <Line className="absolute left-0 w-full -bottom-4" />
           <div className="home-card-wrapper">
-            <Line orientation="vertical" className="absolute -right-4 top-0 h-[30rem]" />
-            <Line orientation="vertical" className="absolute -left-4 bottom-0 h-[30rem]" />
+            <Line
+              orientation="vertical"
+              className="absolute -right-4 top-0 h-[30rem]"
+            />
+            <Line
+              orientation="vertical"
+              className="absolute -left-4 bottom-0 h-[30rem]"
+            />
             <HeroCard
               disabled
               name="Производительность"
@@ -35,20 +49,34 @@ const Hero = () => {
               description="Создайте портфолио загрузив работы в профиль"
               link="https://frame.darkmaterial.space"
             >
-              <Image src='/grid.svg' fill alt='grid' className="object-cover scale-150 opacity-50" />
-              <Image src={cdn('dm/icons/frame-dark.svg')} width={64} height={64} alt='logo' className="z-10 self-center" />
+              <Image
+                src="/grid.svg"
+                fill
+                alt="grid"
+                className="object-cover scale-150 opacity-50"
+              />
+              <Image
+                src={cdn("dm/icons/frame-dark.svg")}
+                width={64}
+                height={64}
+                alt="logo"
+                className="z-10 self-center"
+              />
             </HeroCard>
           </div>
         </div>
       </main>
-    )
-  }
+    );
+  };
   return (
-    <div id="hero" className="w-full h-screen overflow-visible flex flex-col items-center justify-center relative">
+    <div
+      id="hero"
+      className="w-full h-screen overflow-visible flex flex-col items-center justify-center relative"
+    >
       <StarField starsCount={350} />
       <HeroContent />
       <div className="w-full h-72 absolute bottom-0 flex items-end z-[-1]">
-        <Image src='/bottom-light.png' fill alt='light-image' />
+        <Image src="/bottom-light.png" fill alt="light-image" />
         <Line variant="primary" />
       </div>
     </div>
--- app/(home)/_components/home-projects.tsx
+++ app/(home)/_components/home-projects.tsx
@@ -4,32 +4,40 @@ import { all_products_page } from "@/const/all-products";
 import Link from "next/link";
 import { BiChevronRight } from "react-icons/bi";
 
-
 const HomeProjects = () => {
-
-    return (
-        <div id="projects" className="w-full min-h-screen py-36 gap-12 flex flex-col z-10">
-            <section className="max-w-7xl mx-auto w-full px-6 flex flex-col gap-4">
-                <div className="w-full h-fit flex items-center justify-between">
-                    <h1 className='md:text-4xl text-2xl font-bold text-accent-foreground'>Сервисы DM</h1>
-                    <Button variant='ghost' className="gap-2" asChild>
-                        <Link href='/all-products'>Посмотреть все <BiChevronRight /></Link>
-                    </Button>
-                </div>
-                <span className='text-base font-light text-muted-foreground'>
-                    Вы можете воспользоваться приложениями без аккаунта DM Family, но для лучшего
-                    пользовательского опыта, рекомендуется создать аккаунт DM Family, один аккаунт на все сервисы.
-                </span>
-            </section>
-            <div className="max-w-7xl w-full mx-auto px-6 gap-6 h-full">
-            {
-                all_products_page.map(section =>
-                    <ProductsSection key={section.sectionId} section={section} hideTitle />
-                )
-            }
-            </div>
+  return (
+    <div
+      id="projects"
+      className="w-full min-h-screen py-36 gap-12 flex flex-col z-10"
+    >
+      <section className="max-w-7xl mx-auto w-full px-6 flex flex-col gap-4">
+        <div className="w-full h-fit flex items-center justify-between">
+          <h1 className="md:text-4xl text-2xl font-bold text-accent-foreground">
+            Сервисы Darkmaterial
+          </h1>
+          <Button variant="ghost" className="gap-2" asChild>
+            <Link href="/all-products">
+              Посмотреть все <BiChevronRight />
+            </Link>
+          </Button>
         </div>
-    )
-}
+        <span className="text-base font-light text-muted-foreground">
+          Вы можете воспользоваться приложениями без аккаунта Darkmaterial, но
+          для лучшего пользовательского опыта, рекомендуется создать аккаунт
+          Darkmaterial, один аккаунт на все сервисы.
+        </span>
+      </section>
+      <div className="max-w-7xl w-full mx-auto px-6 gap-6 h-full">
+        {all_products_page.map((section) => (
+          <ProductsSection
+            key={section.sectionId}
+            section={section}
+            hideTitle
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default HomeProjects
+export default HomeProjects;
